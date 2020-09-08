ea6107d621ca9380eb15116ca1be9f6aa4ace903
saranDevarajan <saranya.devarajan@lease-works.com>
2020-09-08 07:08:48 +0530

    Initial commit

---
+++ src/main/java/com/learning/bookmark/catalog/BookmarkCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.learning.bookmark.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/bookmark/catalog/BookmarkCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.bookmark.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
