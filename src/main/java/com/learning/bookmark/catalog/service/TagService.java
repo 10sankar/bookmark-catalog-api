06fbccfdf66b5dc12c059fd3cd8c546f74fc4e1e
saranDevarajan <saranya.devarajan@lease-works.com>
2020-09-16 22:28:09 +0530

    feat: added Tag service layer

--- src/main/java/com/learning/bookmark/catalog/service/TagService.java
+++ src/main/java/com/learning/bookmark/catalog/service/TagService.java
@@ -1,7 +1,30 @@
 package com.learning.bookmark.catalog.service;
 
+import com.learning.bookmark.catalog.entity.CardTagRelation;
+import com.learning.bookmark.catalog.entity.Tag;
+import com.learning.bookmark.catalog.repo.CardTagRelationRepository;
+import com.learning.bookmark.catalog.repo.TagRepository;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class TagService {
+
+    private final TagRepository tagRepository;
+    private final CardTagRelationRepository cardTagRelationRepository;
+
+    public Tag save(String tag) {
+        Tag byValue = tagRepository.findByValue(tag);
+        if (byValue == null) {
+            byValue = tagRepository.save(new Tag().setValue(tag));
+        }
+        return byValue;
+    }
+
+    public void deleteTagForCard(Integer cardId) {
+        cardTagRelationRepository.deleteAllByCardId(cardId);
+    }
+
+    public CardTagRelation saveTagForCard(Integer cardId, Integer tagId) {
+        return cardTagRelationRepository.save(new CardTagRelation(cardId, tagId));
+    }
 }
